59072e664c2808e9a4df63317cdde9536b66a92b
tatva-muntazir <muntazirmahendi.bhimani@tatvasoft.com>
2025-06-10 10:52:05 +0530

    Work on implement show camera preview to user and work on implement detect faces and log it

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,27 +1,68 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {useEffect, useState, useRef} from 'react';
+import {
+  Camera,
+  runAsync,
+  useCameraDevice,
+  useFrameProcessor,
+} from 'react-native-vision-camera';
+import {
+  Face,
+  useFaceDetector,
+  FaceDetectionOptions,
+} from 'react-native-vision-camera-face-detector';
+import {Worklets} from 'react-native-worklets-core';
 
-export default function CameraScreen({navigation}) {
+export default function App() {
+  const faceDetectionOptions =
+    useRef <
+    FaceDetectionOptions >
+    {
+      // detection options
+    }.current;
+
+  const device = useCameraDevice('front');
+  const {detectFaces} = useFaceDetector(faceDetectionOptions);
+
+  useEffect(() => {
+    (async () => {
+      const status = await Camera.requestCameraPermission();
+      console.log({status});
+    })();
+  }, [device]);
+
+  const handleDetectedFaces = Worklets.createRunOnJS((faces: Face[]) => {
+    console.log('faces detected', faces);
+  });
+
+  const frameProcessor = useFrameProcessor(
+    frame => {
+      'worklet';
+      runAsync(frame, () => {
+        'worklet';
+        const faces = detectFaces(frame);
+        // ... chain some asynchronous frame processor
+        // ... do something asynchronously with frame
+        handleDetectedFaces(faces);
+      });
+      // ... chain frame processors
+      // ... do something with frame
+    },
+    [handleDetectedFaces],
+  );
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to Camera Screen</Text>
+    <View style={{flex: 1}}>
+      {!!device ? (
+        <Camera
+          style={StyleSheet.absoluteFill}
+          device={device}
+          isActive={true}
+          frameProcessor={frameProcessor}
+        />
+      ) : (
+        <Text>No Device</Text>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f2f6fc',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 26,
-    fontWeight: 'bold',
-    marginBottom: 50,
-    color: '#333',
-    textAlign: 'center',
-  },
-});
